dca61a71318d174e5dbda13cd723c676ecc1be7e
aya <helloaya1984@gmail.com>
2017-06-20 16:18:44 +0800

    1

--- apps/test/main.cpp
+++ apps/test/main.cpp
@@ -7,14 +7,14 @@ int do_client (){
 	printf ("Connecting to hello world server…\n");
     void *context = zmq_ctx_new ();
     void *requester = zmq_socket (context, ZMQ_REQ);
-    zmq_connect (requester, "tcp://localhost:5555");
+    zmq_connect (requester, "tcp://35.185.158.58:5555");
 
     int request_nbr;
     for (request_nbr = 0; request_nbr != 10; request_nbr++) {
         char buffer [10];
         printf ("Sending Hello %d…\n", request_nbr);
         zmq_send (requester, "Hello", 5, 0);
-        sleep (1);  
+        
         zmq_recv (requester, buffer, 10, 0);
         printf ("Received World %d\n", request_nbr);
 
@@ -34,9 +34,7 @@ int do_server () {
         printf ("Wait for resp\n");
         zmq_recv (responder, buffer, 10, 0);
         printf ("Received %s\n", buffer);
-        printf ("Wait for resp2\n");
-        
-        printf ("Received2  %d\n",  zmq_recv (responder, buffer, 10, 0));
+        sleep (1)
         zmq_send (responder, "World", 5, 0);
     }
 	return 0;
