2c956099b588a6627869fe98aa199f0a2106b215
aya <helloaya1984@gmail.com>
2017-06-20 18:33:14 +0800

    4

--- apps/test/pub_sub.cpp
+++ apps/test/pub_sub.cpp
@@ -27,7 +27,7 @@ int do_pub() {
         printf ("sending..\n");
         zmq_send (publisher, update, strlen(update), 0);
         printf ("done...waiting...\n");
-        sleep (5);
+        sleep (1);
     }
     zmq_close (publisher);
     zmq_ctx_destroy (context);
@@ -40,25 +40,26 @@ int do_sub () {
     void *context = zmq_ctx_new ();
     void *subscriber = zmq_socket (context, ZMQ_SUB);
     int rc = zmq_connect (subscriber, "tcp://35.185.158.58:5555");
-
+    printf ("connect %i\n", rc);
     //  Subscribe to zipcode, default is NYC, 10001
     char *filter = "demo";
     rc = zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE,
                          filter, strlen (filter));
 
+    printf ("setsockopt %i\n", rc);
     //  Process 100 updates
     int update_nbr;
     long total_temp = 0;
     for (update_nbr = 0; update_nbr < 100; update_nbr++) {
         char string[20] = {0,};
 
+        printf ("wait for update %i\n", update_nbr);
         zmq_recv (subscriber, string, 20, 0);
 
         int zipcode, temperature, relhumidity;
         sscanf (string, "%d %d %d",
             &zipcode, &temperature, &relhumidity);
         total_temp += temperature;
-        free (string);
     }
     printf ("Average temperature for zipcode '%s' was %dF\n",
         filter, (int) (total_temp / update_nbr));
